8ad95ff4021126a8f0752b62e1d5ef70033b2709
Cucurrucho <adam.bar91@gmail.com>
2019-06-16 01:35:26 +0300

    added reply to

--- app/Notifications/SendContactEmailNotification.php
+++ app/Notifications/SendContactEmailNotification.php
@@ -38,6 +38,7 @@ class SendContactEmailNotification extends Notification {
 	 */
 	public function toMail($notifiable) {
 		return (new MailMessage)
+			->replyTo($this->content['email'])
 			->from($this->content['email'])
 
 			->subject($this->content['subject'])
